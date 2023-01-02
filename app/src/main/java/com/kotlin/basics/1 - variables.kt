f92b2b5dd518f37c3cf398e9759fd55fb39c6994
Abdulrahman Al-Ghamdi <abdulrahman@Abdulrahmans-MacBook-Air.local>
2023-01-02 07:58:01 +0300

    Kotlin Basics: first day content

---
+++ app/src/main/java/com/kotlin/basics/1 - variables.kt
@@ -0,0 +1,17 @@
+package com.kotlin.basics
+
+fun main() {
+
+    // val is immutable variable (cannot be reassigned)
+    val name = "Abdulrahman"
+    println(name)
+
+    // var is mutable variable (can be reassigned)
+    var age = 20
+    println(age)
+
+    // To change a var value, refer to the variable name
+    age = 25
+    println(age)
+
+}
---
+++ app/src/main/java/com/kotlin/basics/2 - datatypes.kt
@@ -0,0 +1,37 @@
+package com.kotlin.basics
+
+fun main() {
+
+    // Byte type: accepts integer value (8 Bits)
+    val byte: Byte = 127
+    println("Byte: $byte")
+
+    // Short type (accepts integer value - 16 Bits)
+    val short: Short = 32767
+    println("Short: $short")
+
+    // Int type (accepts integer value - 32 Bits)
+    val int: Int = 290504040
+    println("Int: $int")
+
+    // Long type (accepts integer value - 32 Bits)
+    val long: Long = 2348927352344521234
+    println("Long: $long")
+
+    // Float type (accepts rational value - 32 Bits)
+    val float: Float = 23.32420f
+    println("Float: $float")
+
+    // Double type (accepts rational value - 64 Bits)
+    val double: Double = 23.324204523522347
+    println("Double: $double")
+
+    // Boolean type (accepts either true or false)
+    val boolean: Boolean = true
+    println("Boolean: $boolean")
+
+    // Char type (accepts character value)
+    val char: Char = 'A'
+    println("Char: $char")
+
+}
