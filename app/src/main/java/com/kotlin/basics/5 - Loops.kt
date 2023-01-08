18eb0736c88a2d2648573560b51bda4c54bd04e0
Abdulrahman Al-Ghamdi <abdulrahman@Abdulrahmans-MacBook-Air.local>
2023-01-08 14:53:40 +0300

    Kotlin Basics: fifth day content

--- app/src/main/java/com/kotlin/basics/5 - Loops.kt
+++ app/src/main/java/com/kotlin/basics/5 - Loops.kt
@@ -1,25 +1,40 @@
 package com.kotlin.basics
 
 fun main() {
-    val student1 = Student("Abdulrahman", "Math")
-    val student2 = Student("Saleh", "CS")
 
-    val school = School()
-    school.printStudent(student2)
-    school.changeSubject(student1, "Programming")
-}
+    // while: loops through a block of code as long as a the condition is true.
 
-data class Student(val name: String, var subject: String)
+    var counter = 0
 
-class School {
-
-    fun printStudent(student: Student) {
-        println("Name: ${student.name}, Subject: ${student.subject}")
+    while (counter < 5) {
+        println("Counter: $counter")
+        counter++
     }
 
-    fun changeSubject(student: Student, subject: String) {
-        println("Old Subject: ${student.subject}")
-        student.subject = subject
-        println("New Subject: ${student.subject}")
+    // do while: execute the code block once before checking the condition
+
+    var temperature = 4
+
+    do {
+        println("Temperature: $temperature")
+        temperature--
+    } while (temperature > 0)
+
+    // for loop: is used to loop through arrays and ranges. It used together with the (in) keyword.
+
+    // (..) is used create range between two numbers (small number to big number)
+    for (number in 0..3) {
+        println("Number: $number")
     }
+
+    // (downTo) is used create range between two numbers (big number to small number)
+    for (season in 4 downTo  1) {
+        println("Season: $season")
+    }
+
+    // (step) is used to go through number with extra steps instead of one step everytime
+    for (month in 1..12 step 2) {
+        println("Month: $month")
+    }
+
 }
